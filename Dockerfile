b6c93f44a13e6beebb4b72ed2eac1e93b0715498
xosadmin <9164356+xosadmin@users.noreply.github.com>
2025-01-07 11:32:48 +0800

    Initial Commit

---
+++ Dockerfile
@@ -0,0 +1,41 @@
+FROM debian:bullseye-slim
+
+ENV DEBIAN_FRONTEND=noninteractive
+
+RUN apt update -y --fix-missing && \
+    apt-get install -y \
+    ca-certificates \
+    curl \
+    gnupg2 \
+    lsb-release \
+    apt-transport-https \
+    vim \
+    cron
+
+RUN apt install nginx -y && \
+    apt-get install php8.3 \
+    php8.3-cli php8.3-cgi php8.3-fpm \
+    php8.3-gd php8.3-mysql php8.3-imap \
+    php8.3-curl php8.3-intl php8.3-pspell \
+    php8.3-sqlite3 php8.3-tidy php8.3-xsl \
+    php8.3-zip php8.3-mbstring php8.3-soap \
+    php8.3-opcache libonig5 php8.3-common \
+    php8.3-readline php8.3-xml -y
+
+RUN apt-get clean && \
+    rm -rf /var/lib/apt/lists/*
+
+COPY default.conf /etc/nginx/sites-available
+
+COPY default-ssl.conf /etc/nginx/sites-available
+
+COPY entrypoint.sh /
+
+RUN chmod a+x /entrypoint.sh
+
+RUN rm -f /etc/nginx/sites-enabled/default && \
+    ln -s /etc/nginx/sites-enabled/default /etc/nginx/sites-available/default.conf
+
+EXPOSE 80 443
+
+ENTRYPOINT ["/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+function installSSL() {
+    domain=$1
+    cert=$2
+    key=$3
+
+    nginxEnabledSitesAddr="/etc/nginx/sites-enabled"
+    nginxAvailSitesAddr="/etc/nginx/sites-available"
+
+    rm -f $nginxEnabledSitesAddr/default
+    ln -s $nginxAvailSitesAddr/default-ssl.conf $nginxEnabledSitesAddr/default-ssl
+
+    sed -i "s/_domain_/$domain/g" $nginxAvailSitesAddr/default-ssl.conf
+    sed -i "s|_ssl_certificate_|$cert|g" $nginxAvailSitesAddr/default-ssl.conf
+    sed -i "s/_ssl_certificate_key_/$key/g" $nginxAvailSitesAddr/default-ssl.conf
+}
+
+function adjustPHP() {
+    if [ ! -f /etc/nginx/init.lock ]; then
+        sed -i "s/memory_limit = .*/memory_limit = 256M/" /etc/php/8.3/fpm/php.ini
+        sed -i "s/upload_max_filesize = .*/upload_max_filesize = 128M/" /etc/php/8.3/fpm/php.ini
+        sed -i "s/post_max_size = .*/post_max_size = 128M/" /etc/php/8.3/fpm/php.ini
+        sed -i "s/max_execution_time = .*/max_execution_time = 90/" /etc/php/8.3/fpm/php.ini
+    else
+        echo "PHP is already initialised. Skip initialisation..."
+    fi
+}
+
+initlock=$(cat /etc/nginx/init.lock 2>/dev/null || echo 0)
+
+if [ ! -z "$ssl" ] && [ ! -z "$domain" ] && [ $initlock -eq 0 ]; then
+    if [ ! -z "$email" ]; then
+        curl https://get.acme.sh | sh -s email=$email
+        if [ -d /root/.acme.sh ]; then
+            echo "0 0 * * * /root/.acme.sh/acme.sh --cron --home '/root/.acme.sh' > /dev/null" >> /etc/cron.d/cronjobs
+            chmod 644 /etc/cron.d/cronjobs
+
+            /root/.acme.sh/acme.sh --issue -d $domain --standalone
+            if [ -f /root/.acme.sh/$domain/fullchain.cer ]; then
+                installSSL "$domain" "/root/.acme.sh/$domain/fullchain.cer" "/root/.acme.sh/$domain/$domain.key"
+                echo "Done SSL installation."
+                adjustPHP
+                echo "1" > /etc/nginx/init.lock
+            else
+                echo "Certbot issue cert failed."
+                exit 1
+            fi
+        else
+            echo "acme.sh install failed."
+            exit 1
+        fi
+    else
+        if [ ! -d /etc/nginx/ssl ]; then
+            echo "No SSL found"
+            exit 1
+        else
+            if [ -f /etc/nginx/ssl/server.crt ] && [ -f /etc/nginx/ssl/server.key ]; then
+                installSSL "$domain" "/etc/nginx/ssl/server.crt" "/etc/nginx/ssl/server.key"
+                echo "Done SSL installation."
+            else
+                echo "Cannot find certificate."
+                exit 1
+            fi
+        fi
+    fi
+else
+    if [ $initlock -eq 1 ]; then
+        echo "Nginx already initialised. Skip initialisation..."
+    else
+        echo "Skip SSL installation."
+        sed -i "s/_domain_/$domain/g" /etc/nginx/sites-available/default.conf
+        adjustPHP
+        echo "1" > /etc/nginx/init.lock
+    fi
+fi
+
+if [ ! -d /var/www/html ]; then
+    mkdir -p /var/www/html
+    chmod -R 755 /var/www/html
+fi
+
+php-fpm8.3 -F &
+cron &
+nginx -g "daemon off;"
